b00ae88c0dc02a62a2e026fbd72542105a975724
Luckykandpal021 <luckykandpal059@gmail.com>
2023-07-03 10:02:24 +0530

    Question1 3 July

--- LoopQuestion.java
+++ LoopQuestion.java
@@ -1,15 +1,40 @@
+
+// Question for a = a + (int) Math.pow(2, j) * b;
 import java.util.Scanner;
 
-// Multiplication by Input
-public class LoopQuestion {
-    public static void main(String[] args) {
+class LoopQuestion {
+    public static void main(String[] argh) {
         Scanner userInput = new Scanner(System.in);
-        System.out.print("Enter Your Multiplication Number:- ");
-        int n = userInput.nextInt();
-        for (int i = 1; i <= 10; i++) {
-            System.out.println(n + " x " + i + " = " + n * i);
+        int t = userInput.nextInt();
+
+        for (int i = 0; i < t; i++) {
+            int a = userInput.nextInt();
+            int b = userInput.nextInt();
+            int n = userInput.nextInt();
+
+            for (int j = 0; j < n; j++) {
+                a = a + (int) Math.pow(2, j) * b;
+                System.out.print(a + " ");
+            }
+            System.out.println("");
         }
-        userInput.close(); // Close the Scanner object
+        userInput.close();
 
     }
 }
+
+// // Multiplication by Input
+
+// import java.util.Scanner;
+// public class LoopQuestion {
+// public static void main(String[] args) {
+// Scanner userInput = new Scanner(System.in);
+// System.out.print("Enter Your Multiplication Number:- ");
+// int n = userInput.nextInt();
+// for (int i = 1; i <= 10; i++) {
+// System.out.println(n + " x " + i + " = " + n * i);
+// }
+// userInput.close(); // Close the Scanner object
+
+// }
+// }
