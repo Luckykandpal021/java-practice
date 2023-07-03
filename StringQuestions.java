708a1859f435d2b9300bde38a57f923fe71d7b21
Luckykandpal021 <luckykandpal059@gmail.com>
2023-07-03 11:07:22 +0530

    Question2 SubString HR

--- StringQuestions.java
+++ StringQuestions.java
@@ -10,19 +10,38 @@ Double: 3.1415
 Int: 42
 
 */
+// import java.util.Scanner;
+
+// class StringQuestions {
+//     public static void main(String[] args) {
+//         Scanner userInput = new Scanner(System.in);
+//         int intValue = userInput.nextInt();
+//         double doubleValue = userInput.nextDouble();
+//         userInput.nextLine();
+//         String stringValue = userInput.nextLine();
+//         System.out.println("String: " + stringValue);
+//         System.out.println("Double: " + doubleValue);
+//         System.out.println("Int: " + intValue);
+
+//         userInput.close();
+//     }
+// }
+
+// Sample input:-
+// Helloworld
+// 3 7
+// Sample Output:- lowo
+
 import java.util.Scanner;
 
 class StringQuestions {
     public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
-        int intValue = userInput.nextInt();
-        double doubleValue = userInput.nextDouble();
-        userInput.nextLine();
         String stringValue = userInput.nextLine();
-        System.out.println("String: " + stringValue);
-        System.out.println("Double: " + doubleValue);
-        System.out.println("Int: " + intValue);
-
+        int intValue1 = userInput.nextInt();
+        int inputValue2 = userInput.nextInt();
+        System.out.println(stringValue.substring(intValue1, inputValue2));
         userInput.close();
+
     }
 }
