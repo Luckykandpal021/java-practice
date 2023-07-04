54492e17666fe56ebadf4cfd7882c49652128868
Luckykandpal021 <luckykandpal059@gmail.com>
2023-07-04 10:08:40 +0530

    New Java File for Regex

---
+++ RegxPractice.java
@@ -0,0 +1,12 @@
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+class RegxPractice {
+    public static void main(String[] args) {
+        Pattern definePattern = Pattern.compile("Lucky", Pattern.CASE_INSENSITIVE); // defining a pattern
+        Matcher matcher = definePattern.matcher("I am lcky, because I am luck"); // Used to Search for the Pattern
+        boolean result = matcher.find(); // For Finding a Result for a Pattern
+        System.out.println("Result:- " + result);
+
+    }
+}
